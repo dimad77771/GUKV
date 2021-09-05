67a00ae80143f990f5bddf55f378293d000dff0b
dima7777 <dima7777@DESKTOP-KCF43RE>
2021-09-06 00:10:41 +0300

    -

---
+++ ReportWebSite/App_Code/ZvitDbUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Xml;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ExtDataEntry.Models;
+using System.Data.SqlClient;
+
+public static class ZvitDbUtils
+{
+	public static void DbFilename2LocalFilename(SqlConnection connection, int reportID, string user)
+	{
+		WorkItem workItem = null;
+
+		workItem.Init(reportID, user);
+		workItem.DoWork();
+
+		//var trans = connection.BeginTransaction();
+
+		//Reports1NFUtils.SendRentedObject(connection, reportID, id);
+
+		//trans.Commit();
+	}
+
+}
