d8b82a8c531422eeb45e82873243acca7c21423d
dimad77771-dev <80140265+dimad77771-dev@users.noreply.github.com>
2022-05-23 02:36:48 +0300

    -

--- ReportWebSite/Reports1NF/Report1NFFreeMap.aspx.cs
+++ ReportWebSite/Reports1NF/Report1NFFreeMap.aspx.cs
@@ -16,6 +16,10 @@ public partial class Reports1NF_Report1NFFreeMap : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
     {
+		//TEMP_DISABLE_MAP
+		Response.Redirect("Report1NFFreeShow.aspx");
+
+
 		var url = "Report1NFFreeMap__" + MapUtils.GetSystem() + ".aspx" + Request.Url.Query;
 		Response.Redirect(Page.ResolveClientUrl(url));
 	}
--- ReportWebSite/Reports1NF/Report1NFProdlenMap.aspx.cs
+++ ReportWebSite/Reports1NF/Report1NFProdlenMap.aspx.cs
@@ -16,6 +16,9 @@ public partial class Reports1NF_Report1NFProdlenMap : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
+		//TEMP_DISABLE_MAP
+		Response.Redirect("Report1NFProdlenShow.aspx");
+
 		var url = "Report1NFProdlenMap__" + MapUtils.GetSystem() + ".aspx" + Request.Url.Query;
 		Response.Redirect(Page.ResolveClientUrl(url));
 	}
