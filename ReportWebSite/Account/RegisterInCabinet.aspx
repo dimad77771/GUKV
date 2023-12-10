8a37222152cbf979b7362036a35b367f9577e30b
pbs-dev-01 <pbs-dev-01@pbs-dev-01.com>
2023-12-10 09:07:01 +0200

    -

--- ReportWebSite/Account/RegisterInCabinet.aspx
+++ ReportWebSite/Account/RegisterInCabinet.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Реєстрація нового користувача" Language="C#" MasterPageFile="~/NoHeader.master" AutoEventWireup="true"
+﻿<%@ Page Title="Реєстрація нового користувача" Language="C#" MasterPageFile="~/LoginInCabinet.master" AutoEventWireup="true"
     CodeFile="RegisterInCabinet.aspx.cs" Inherits="Account_Register" %>
 
 <%@ Register assembly="DevExpress.Web.v20.1, Version=20.1.3.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web" tagprefix="dx" %>
---
+++ ReportWebSite/LoginInCabinet.master.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class FreeShowPublic : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+		var menu1 = LoginView1.FindControlRecursive("LoginMenu1") as DevExpress.Web.ASPxMenu;
+		if (menu1 != null)
+		{
+			var menuitem = menu1.Items.Single(q => q.Name == "LoginInCabinet");
+			menuitem.NavigateUrl += "?ReturnUrl=" + Request.Url.AbsolutePath;
+		}
+
+		var menu2 = LoginView1.FindControlRecursive("LoginMenu2") as DevExpress.Web.ASPxMenu;
+		if (menu2 != null)
+		{
+			var menuitem = menu2.Items.Single(q => q.Name == "LogoutCabinet");
+			menuitem.NavigateUrl += "?ReturnUrl=" + Request.Url.AbsolutePath;
+		}
+	}
+
+	protected void ASPChangeMapSystem_Click(object sender, EventArgs e)
+	{
+		MapUtils.SetSystem(MapUtils.MBK);
+		Response.Redirect("Report1NFFreeMap.aspx" + Request.Url.Query);
+	}
+}
