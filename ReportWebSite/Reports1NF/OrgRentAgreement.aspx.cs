b20522e2e5632ca409cdd312c8d16cd4e22f95da
pbs-dev-01 <pbs-dev-01@pbs-dev-01.com>
2025-03-13 05:48:35 +0200

    -

--- ReportWebSite/Reports1NF/OrgRentAgreement.aspx.cs
+++ ReportWebSite/Reports1NF/OrgRentAgreement.aspx.cs
@@ -3971,21 +3971,18 @@ public class NarazhCalculation
 			{
 				var date = new DateTime(year, mm, 1);
 				var inflation = GetMonthInflation(date);
-				var plata0 = plata;
+
+				if (isnew && date == baseMonth && rentStart <= baseMonth.AddMonths(1))
+				{
+					inflation = 100M;
+				}
 
 				if (isnew)
 				{
 					plata = round_0(plata * inflation / 100M);
 				}
 
-				if (isnew && date == baseMonth && rentStart <= baseMonth.AddMonths(1))
-				{
-					monthPlata.Add(date, plata0);
-				}
-				else
-				{
-					monthPlata.Add(date, plata);
-				}
+				monthPlata.Add(date, plata);
 
 				if (!isnew)
 				{
